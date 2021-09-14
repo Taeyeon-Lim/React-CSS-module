9c19291e964fe1ce99f6662d9720e5ebe0d694ca
Teayeon-Lim <wingofyeon@naver.com>
2021-09-14 17:48:10 +0900

    feature/install react-icons lib

---
+++ src/components/CheckBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+
+function CheckBox({ checked, children, ...rest }) {
+  return (
+    <div>
+      <label>
+        <input type='checkbox' checked={checked} {...rest} />
+        <div>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        </div>
+      </label>
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export default CheckBox;
